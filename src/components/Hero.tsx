16398f0ccd426e5fa1af8b18d9548a00eb3a5dca
sgcuentaoficial <sgcuentaoficialdemos@gmail.com>
2025-09-18 01:33:02 +0200

    Eliminar completamente el botón de scroll que rebota

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -202,13 +202,6 @@ const Hero = () => {
           </div>
         </div>
       </div>
-
-      {/* Scroll Indicator */}
-      <div className="absolute bottom-8 md:bottom-12 left-1/2 transform -translate-x-1/2 animate-bounce z-20">
-        <div className="w-6 h-10 border border-white/30 rounded-full flex justify-center backdrop-blur-sm">
-          <div className="w-1 h-3 bg-vip-gold rounded-full mt-2 animate-pulse"></div>
-        </div>
-      </div>
     </section>
   );
 };
